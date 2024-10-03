cd2981b1593490812471157149ff48fa06dcf3ac
Bhuvaneshwari-M05 <bhuvaneshwarideveloper@gmail.com>
2024-10-03 22:00:04 +0530

    Deploying to GitHub Pages

---
+++ assets/ErrorPage-8CC_Qdy_.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DHjt6gqb.js";const a=()=>r.jsx(r.Fragment,{children:r.jsx("div",{className:"2xl:container mx-auto",children:r.jsx("div",{className:"w-[90%] mx-auto ",children:r.jsx("div",{})})})});export{a as default};
